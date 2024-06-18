e7cbcea126274c77bf8b74f09b55aa566df2c07e
Alexandre Machado <alexandre@machado.cc>
2024-06-17 23:54:31 -0300

    add play wrigth

---
+++ tests/tests.ts
@@ -0,0 +1,8 @@
+import { test } from '@playwright/test';
+
+test('Page Screenshot', async ({ page }) => {
+  await page.goto('https://alexandre.machado.cc/');
+  
+  await expect(page).toHaveTitle(/Alexandre Machado/);
+  await page.screenshot({ path: `print.png` });
+});
