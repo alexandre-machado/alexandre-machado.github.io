ea919a2e32cafdc8ad324fae45b553b0df6d975c
Alexandre Machado <alexandre@machado.cc>
2024-06-18 01:12:00 -0300

    chore: Add JS error detection in test

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,8 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('Page Screenshot', async ({ page, browser  }) => {
-  await page.goto('https://alexandre.machado.cc/');
+  await page.goto('https://alexandre.machado.cc');
   
   await expect(page).toHaveTitle(/Alexandre Machado/);
   await page.screenshot({ path: `test-results/${browser.browserType().name()}/print.png` });
+});
+
+test('detect JS errors', async ({ page }) => {
+  page.on('console', msg => {
+    if (msg.type() === 'error') {
+      console.log(`Erro de JS detectado: ${msg.text()}`);
+    }
+  });
+
+  await page.goto('https://alexandre.machado.cc');
 });
