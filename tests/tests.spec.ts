8d59fa195d5133e029e6ba6b91652883bb3aef85
Alexandre Machado <alexandre@machado.cc>
2024-06-18 00:55:40 -0300

    add path

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('Page Screenshot', async ({ page }) => {
+test('Page Screenshot', async ({ page, browser  }) => {
   await page.goto('https://alexandre.machado.cc/');
   
   await expect(page).toHaveTitle(/Alexandre Machado/);
-  await page.screenshot({ path: `print.png` });
+  await page.screenshot({ path: `test-results/${browser.browserType().name()}/print.png` });
 });
