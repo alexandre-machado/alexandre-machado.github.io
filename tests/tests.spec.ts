1db0532d9243bdeb2cb36fa92b77534fb5fbd057
Alexandre Machado <alexandre@machado.cc>
2024-06-18 00:36:25 -0300

    fix

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('Page Screenshot', async ({ page }) => {
   await page.goto('https://alexandre.machado.cc/');
