07d42fd1b63b7274aa0458703c63edeed190a6e2
Alexandre Machado <alexandre@machado.cc>
2025-01-18 02:00:06 -0300

    chore: Update README and Playwright configuration, add test scripts

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # alexandre-machado.github.io
 
 Site: https://alexandre.machado.cc/
+
+# tests
+
+## Run/Updating Playwright
+```bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+nvm install --latest-npm 20.0.0
+nvm use 20.0.0
+npm install -D @playwright/test@latest
+npx playwright install --with-deps
+npx playwright test
+```
--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,18 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('Page Screenshot', async ({ page, browser  }) => {
-  await page.goto('https://alexandre.machado.cc');
-  
-  await expect(page).toHaveTitle(/Alexandre Machado/);
-  await page.screenshot({ path: `test-results/${browser.browserType().name()}/print.png` });
-});
+[
+  { testEnvironment: 'E2E', baseUrl: 'https://alexandre.machado.cc' },
+  { testEnvironment: 'Local', baseUrl: './' },
+].forEach(({ testEnvironment, baseUrl }) => {
+  test(`${testEnvironment}: Page Screenshot`, async ({ page, browser }) => {
+    await page.goto(baseUrl);
 
-test('detect JS errors', async ({ page }) => {
-  page.on('console', msg => {
-    if (msg.type() === 'error') {
-      console.log(`Erro de JS detectado: ${msg.text()}`);
-    }
+    await expect(page).toHaveTitle(/Alexandre Machado/);
+    await page.screenshot({ path: `test-results/${browser.browserType().name()}/print.png` });
   });
 
-  await page.goto('https://alexandre.machado.cc');
+  test(`${testEnvironment}: detect JS errors`, async ({ page }) => {
+    page.on('console', msg => {
+      if (msg.type() === 'error') {
+        console.log(`Erro de JS detectado: ${msg.text()}`);
+      }
+    });
+
+    await page.goto(baseUrl);
+  });
+
 });
